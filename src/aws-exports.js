045db56e667d62f28fa6fa8c84932130498b9ab2
Manickaraj K M <manickgm@amazon.com>
2022-06-25 09:33:58 +0530

    testing

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -3,26 +3,12 @@
 
 const awsmobile = {
     "aws_project_region": "us-east-1",
-    "aws_cognito_identity_pool_id": "us-east-1:e7ae2837-096b-4406-9599-ab31d133a5ac",
-    "aws_cognito_region": "us-east-1",
-    "aws_user_pools_id": "us-east-1_8CYUMJCef",
-    "aws_user_pools_web_client_id": "3jmlqrn5lric1jtlgppic4o10k",
-    "oauth": {},
-    "aws_cognito_username_attributes": [],
-    "aws_cognito_social_providers": [],
-    "aws_cognito_signup_attributes": [
-        "EMAIL"
-    ],
-    "aws_cognito_mfa_configuration": "OFF",
-    "aws_cognito_mfa_types": [
-        "SMS"
-    ],
-    "aws_cognito_password_protection_settings": {
-        "passwordPolicyMinLength": 8,
-        "passwordPolicyCharacters": []
-    },
-    "aws_cognito_verification_mechanisms": [
-        "EMAIL"
+    "aws_dynamodb_all_tables_region": "us-east-1",
+    "aws_dynamodb_table_schemas": [
+        {
+            "tableName": "postsforamplify-dev",
+            "region": "us-east-1"
+        }
     ]
 };
 
